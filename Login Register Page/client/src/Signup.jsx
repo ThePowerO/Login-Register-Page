6da974ec5fb8c041d1ed130cbe58e68e63f5f0ce
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-09-30 23:12:10 -0300

    Add files via upload

---
+++ Login Register Page/client/src/Signup.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './Signup.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import ImageL from './assets/img/Site-logo.png';
+import ImageBG from './assets/img/BF-BG.png';
+import { Link } from 'react-router-dom';
+
+function Signup() {
+    const [rememberMeChecked, setRememberMeChecked] = useState(false);
+
+    const toggleRememberMe = () => {
+        setRememberMeChecked(!rememberMeChecked);
+    };
+
+    return (
+        <div>
+            <img className='image-bf' src={ImageBG} />
+                <div className='container'>
+                <div className='container-box'>
+                    <form className='form'>
+                        <img className='logo-site' src={ImageL} />
+                        <h1>Register</h1>
+                        <div className='input-box'>
+                            <span className='icon'><i className="bi bi-person"></i></span>
+                            <input type="text" name='nickname' placeholder='Nickname' required />
+                        </div>
+                        <div className='input-box'>
+                            <span className='icon'><i className="bi bi-envelope"></i></span>
+                            <input type="email" name='email' placeholder='Email' required />
+                        </div>
+                        <div className='input-box'>
+                            <span className='icon'><i className="bi bi-key"></i></span>
+                            <input type="password" name='password' placeholder='Password' required />
+                        </div>
+                        <div id='settings-create' className="d-flex justify-content-between align-items-center">
+                            <div>
+                                <input
+                                    type='checkbox'
+                                    name='rememberme'
+                                    className='remember-me'
+                                    checked={rememberMeChecked}
+                                />
+                                    <span className='remember-me-span' onClick={toggleRememberMe}>
+                                        Remember me
+                                    </span>
+                            </div>
+                            <Link to='/login'>Already Have an account?</Link>
+                        </div>
+                        <button className='btn btn-primary'>Create</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Signup;
