d3e767c789d11cf0693c1ccb371bcd1d4cca1c29
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-09-30 23:17:38 -0300

    Update Login.jsx

--- Login Register Page/client/src/Login.jsx
+++ Login Register Page/client/src/Login.jsx
@@ -33,3 +33,4 @@ function Login() {
                             <div>
                                 <input
                                     type='checkbox'
+                                    name='rememberme'
