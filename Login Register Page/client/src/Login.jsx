9434ebd43f83303a5b60cd842657cd6b5403dde7
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-09-30 23:13:40 -0300

    Update Login.jsx

--- Login Register Page/client/src/Login.jsx
+++ Login Register Page/client/src/Login.jsx
@@ -3,3 +3,4 @@ import './Signup.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import ImageL from './assets/img/Site-logo.png';
 import ImageBG from './assets/img/BF-BG.png';
+import { Link } from 'react-router-dom';
